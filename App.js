0d8465c4052103693272d71635033822061934ad
harish_greavesindia <hrajput@web-fares.com>
2018-03-21 19:34:46 +0530

    Blink Text Using STate

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 5000);
+  }
+
   render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <Text>{display}</Text>
     );
   }
 }
@@ -12,10 +25,11 @@ class Greeting extends Component {
 export default class App extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center' , marginTop: 'auto', marginBottom: 'auto'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
