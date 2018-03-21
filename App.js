ebaaea3281d818d52a55ad49b874e269a7d6cdf6
harish_greavesindia <hrajput@web-fares.com>
2018-03-21 19:45:14 +0530

    Flex Layout Strech Added

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View , Text} from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      // Try setting `flexDirection` to `column`.
+      <View style={{flex: 1, justifyContent: 'space-around', alignItems: 'stretch'}}>
+        <View style={{height: 50, backgroundColor: 'powderblue'}}>
+        </View>
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
-}
+};
